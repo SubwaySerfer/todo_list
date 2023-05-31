1948bec708479576e3c80a68952db5a7989d09e6
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-05-31 22:20:47 +0300

    feat: add todo cards

--- src/components/todos/TodoItems.vue
+++ src/components/todos/TodoItems.vue
@@ -1,7 +1,43 @@
 <template>
-  <section class="container">Items</section>
+  <section class="container">
+    <ul>
+      <base-card v-for="task in tasksData" :key="task.title" class="card">
+        <div class="header-box">
+          <div class="title-box">
+            <h2>Title:</h2>
+            <span class="title">{{ task.title }}</span>
+          </div>
+          <div class="header-box_info">
+            <span class="priority">Priority: {{ task.priority }}</span>
+            <span>Deadline: {{ task.days }}</span>
+          </div>
+        </div>
+
+        <h3>Description:</h3>
+        <p>{{ task.description }}</p>
+      </base-card>
+    </ul>
+  </section>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      tasksArr: [],
+    };
+  },
+  computed: {
+    tasksData() {
+      return this.$store.getters['todo/tasks'];
+    },
+  },
+  created() {
+    this.tasksArr = this.tasksData;
+    console.log(this.tasksArr);
+  },
+};
+</script>
 <style scoped>
 .container {
   width: 60vw;
@@ -9,5 +45,32 @@
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 0.5rem;
+}
+h2,
+h3 {
+  margin: 0;
+}
+.header-box {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.title-box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+}
+.title-box span {
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+.priority {
+  justify-self: center;
+}
+.header-box_info {
+  display: flex;
+  flex-direction: column;
 }
 </style>
