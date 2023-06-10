bae8b20f95f8ec466137be789493c36803c169f2
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-06-10 22:45:41 +0300

    feat: add basics requsts cards

--- src/components/requests/RequestsItems.vue
+++ src/components/requests/RequestsItems.vue
@@ -3,20 +3,73 @@
     <div class="requests-container">
       <h2>Requests:</h2>
       <ul>
-        <li>test</li>
+        <li
+          v-for="request in requests"
+          :key="request.name"
+          class="request-card"
+        >
+          <h3>{{ request.name }}</h3>
+          <a v-if="request.urlGitHub" :href="request.urlGitHub" target="_blank"
+            >{{ request.name }} GitHub!</a
+          >
+          <h5>Text:</h5>
+          <p>{{ request.text }}</p>
+          <!-- {{ request }} -->
+        </li>
       </ul>
     </div>
   </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      requests: [
+        {
+          name: 'alex',
+          tg: '@asdasd',
+          text: 'my Text here',
+          urlGitHub: 'https://github.com/SubwaySerfer',
+        },
+        { name: 'dmitro', tg: '@aaaa', text: 'Test text 2' },
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 .requests-container {
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   width: 70vw;
+  margin-left: 1vw;
+  padding: 0.5rem;
 }
 h2,
-ul {
+ul,
+h3,
+h4,
+p,
+h5 {
   margin: 0;
+}
+h2 {
+  padding: 0.5rem 0 0.5rem 1rem;
+}
+ul {
+  padding-left: 0;
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+}
+.request-card {
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 0.5rem;
+}
+a {
+  text-decoration: none;
 }
 </style>
