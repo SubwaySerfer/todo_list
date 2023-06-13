2dbca3ef9f8e7a93fc1e32b553d3e039b0f60cc1
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-06-13 11:36:05 +0300

    feat: add requests module for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from 'vuex';
 
 import todoModule from './modules/todo/index';
 import homeModule from './modules/home/index';
+import requestsModule from './modules/requests/index';
 
 const store = createStore({
   modules: {
     todo: todoModule,
     home: homeModule,
+    requests: requestsModule,
   },
 });
 
